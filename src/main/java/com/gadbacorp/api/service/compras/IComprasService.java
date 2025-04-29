4bff962491b4bc2ce52b132c7c86d85ce3958341
Alexander <ja.gaslacma@unsm.edu.pe>
2025-04-29 11:08:43 -0500

    Creacion del repositorio

---
+++ src/main/java/com/gadbacorp/api/service/compras/IComprasService.java
@@ -0,0 +1,5 @@
+package com.gadbacorp.api.service.compras;
+
+public interface IComprasService {
+
+}
---
+++ src/main/java/com/gadbacorp/api/service/jpa/compras/ComprasService.java
@@ -0,0 +1,5 @@
+package com.gadbacorp.api.service.jpa.compras;
+
+public class ComprasService {
+
+}
