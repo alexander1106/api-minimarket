ee27c3a51146e27386789e0b8a6ece521fb868a5
carhuajulca <carhuajulca@outlook.es>
2025-05-22 13:17:34 -0500

    creando las tablas para seguridad 0.2

--- src/main/java/com/gadbacorp/api/entity/usuarios/Permissions.java
+++ src/main/java/com/gadbacorp/api/entity/usuarios/Permissions.java
@@ -1,5 +1,10 @@
 package com.gadbacorp.api.entity.usuarios;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table ( name = "permissions" )
 public record Permissions() {
 
 }
--- src/main/java/com/gadbacorp/api/entity/usuarios/Users_Roles.java
+++ src/main/java/com/gadbacorp/api/entity/usuarios/Users_Roles.java
@@ -1,5 +1,10 @@
 package com.gadbacorp.api.entity.usuarios;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users_roles")
 public class Users_Roles {
 
 }
