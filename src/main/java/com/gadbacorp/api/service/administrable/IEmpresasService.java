5f855288f646bc4885ffb3aa762d08e23c9701b0
Alexander <ja.gaslacma@unsm.edu.pe>
2025-06-09 22:36:49 -0500

    integracion con el modulo administrable

--- src/main/java/com/gadbacorp/api/service/administrable/IEmpresasService.java
+++ src/main/java/com/gadbacorp/api/service/administrable/IEmpresasService.java
@@ -4,18 +4,10 @@ import java.util.List;
 import java.util.Optional;
 
 import com.gadbacorp.api.entity.administrable.Empresas;
-
-
 public interface IEmpresasService {
-
     List<Empresas> buscarTodos();
-   
     void guardar(Empresas empresa);
-    
     void modificar(Empresas empresa);
-
     Optional<Empresas> buscarId(Integer id);
-
     void eliminar(Integer id);
-
 }
